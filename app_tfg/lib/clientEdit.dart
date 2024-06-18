e1edca0b03387c9b162450f1ce061def541b31ef
Julia Cano <juliacanoflor@gmail.com>
2024-06-18 19:18:20 +0200

    editar Cliente

---
+++ app_tfg/lib/clientEdit.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'utils/common.dart';
+
+class EditClient extends StatefulWidget {
+  @override
+  final String clientId;
+
+  EditClient({required this.clientId});
+
+  _EditarDatosScreenState createState() => _EditarDatosScreenState();
+}
+
+class _EditarDatosScreenState extends State<EditClient> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController commentsController = TextEditingController();
+  final TextEditingController moneyController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadClientData(widget.clientId);
+  }
+
+  Future<void> _loadClientData(String clientId) async {
+    final clientData =
+        await client.from('clientes').select().eq('id', clientId).single();
+
+    setState(() {
+      nameController.text = clientData['nombre'] ?? '';
+      phoneController.text = clientData['telefono'] ?? '';
+      commentsController.text = clientData['comentario'] ?? '';
+      moneyController.text = clientData['cartera'] ?? '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Editar datos'),
+      ),
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Editar cliente',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 48.0),
+              Container(
+                width: 400.0, // Ajusta el ancho del TextField
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'El campo de nombre no puede estar vacío';
+                    }
+                    return null;
+                  },
+                  controller: nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                width: 400.0, // Ajusta el ancho del TextField
+                child: TextFormField(
+                  controller: phoneController,
+                  decoration: const InputDecoration(
+                    labelText: 'Número de teléfono',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                width: 400.0, // Ajusta el ancho del TextField
+                child: TextFormField(
+                  controller: commentsController,
+                  decoration: const InputDecoration(
+                    labelText: 'Comentario',
+                    border: OutlineInputBorder(),
+                  ),
+                  maxLines: 3,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Container(
+                width: 400.0, // Ajusta el ancho del TextField
+                child: TextFormField(
+                  controller: moneyController,
+                  decoration: const InputDecoration(
+                    labelText: 'Monedero',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    // Validación pasada, proceder con la lógica de añadir cliente
+
+                    bool success = await _editClient(
+                      widget.clientId,
+                      nameController.text,
+                      phoneController.text,
+                      commentsController.text,
+                      moneyController.text,
+                    );
+
+                    if (success) {
+                      Navigator.pop(context, true);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Error al actualizar el cliente'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+
+                    // Limpiar los campos después de añadir el cliente
+                    nameController.clear();
+                    phoneController.clear();
+                    commentsController.clear();
+                    moneyController.clear();
+                  }
+                },
+                child: Text('Editar cliente'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  /*
+      
+      
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: phoneController,
+              decoration: InputDecoration(
+                labelText: 'No hay número de teléfono',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: commentsController,
+              decoration: InputDecoration(
+                labelText: 'No hay comentario',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: moneyController,
+              decoration: InputDecoration(
+                labelText: '0€',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 32),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    bool success = await _editClient(
+                      widget.clientId,
+                      nameController.text,
+                      phoneController.text,
+                      commentsController.text,
+                      moneyController.text,
+                    );
+
+                    if (success) {
+                      Navigator.pop(context, true);
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Error al actualizar el cliente'),
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Aceptar'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }*/
+}
+
+Future<bool> _editClient(String clientId, String name, String phone,
+    String comments, String money) async {
+  try {
+    await client.from('clientes').update({
+      'nombre': name,
+      'telefono': phone,
+      'comentario': comments,
+      'cartera': money
+    }).eq('id', clientId);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
--- app_tfg/lib/clientView.dart
+++ app_tfg/lib/clientView.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'utils/common.dart';
 import 'clientRegister.dart';
+import 'clientEdit.dart';
 
 class ClientView extends StatelessWidget {
   @override
@@ -42,10 +43,6 @@ class _ClienteScreenState extends State<ClienteScreen> {
 
   @override
   Widget build(BuildContext context) {
-    //final clientStream = client.from('clientes').stream(primaryKey: ['id']);
-
-    //clientStream = client.from('clientes').stream(primaryKey: ['id']);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Nuestros clientes'),
@@ -98,8 +95,24 @@ class _ClienteScreenState extends State<ClienteScreen> {
                               mainAxisSize: MainAxisSize.min,
                               children: [
                                 IconButton(
-                                  onPressed: () {
-                                    // DIRIGIR A PÁGINA DE EDITAR CLIENTE
+                                  onPressed: () async {
+                                    final result = await Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              EditClient(clientId: clientId)),
+                                    );
+
+                                    // Actualizar el stream si se registró un nuevo cliente
+                                    if (result == true) {
+                                      clientStream = Supabase.instance.client
+                                          .from('clientes')
+                                          .stream(primaryKey: ['id']);
+
+                                      setState(() {
+                                        // Forzar la reconstrucción del widget con el nuevo stream
+                                      });
+                                    }
                                   },
                                   icon: const Icon(Icons.edit),
                                 ),
@@ -206,7 +219,7 @@ class _ClienteScreenState extends State<ClienteScreen> {
     );
   }
 
-  Future<bool> _editClient(String clientId, String name, String phone,
+  /* Future<bool> _editClient(String clientId, String name, String phone,
       String comments, String money) async {
     try {
       await Supabase.instance.client.from('clientes').update({
@@ -219,7 +232,7 @@ class _ClienteScreenState extends State<ClienteScreen> {
     } catch (e) {
       return false;
     }
-  }
+  }*/
 
   Future<bool> _deleteClient(String clientId) async {
     try {
@@ -238,238 +251,3 @@ class _ClienteScreenState extends State<ClienteScreen> {
     }
   }
 }
-
-
-
-/*import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-import 'utils/common.dart';
-import 'clientRegister.dart';
-
-class ClientView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pantalla Clientes',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ClienteScreen(),
-    );
-  }
-}
-
-class ClienteScreen extends StatefulWidget {
-  @override
-  _ClienteScreenState createState() => _ClienteScreenState();
-}
-
-class _ClienteScreenState extends State<ClienteScreen> {
-  final TextEditingController _searchController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    final clientStream = client.from('clientes').stream(primaryKey: ['id']);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Nuestros clientes'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _searchController,
-                    decoration: InputDecoration(
-                      labelText: 'Buscar',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(),
-                    ),
-                    onChanged: (value) {
-                      setState(() {});
-                    },
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.attach_money),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            StreamBuilder<List<Map<String, dynamic>>>(
-                stream: clientStream,
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-
-                  final clients = snapshot.data!;
-
-                  return ListView.builder(
-                      itemCount: clients.length,
-                      itemBuilder: (context, index) {
-                        final client = clients[index];
-                        final clientId =
-                            client['id'].toString(); //FALTA UN TOSTRING
-
-                        return ListTile(
-                          title: Text(client['nombre']),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                onPressed: () {
-                                  //DIRIGIR A PÁGINA DE EDITAR CLIENTE
-                                },
-                                icon: const Icon(Icons.edit),
-                              ),
-                              IconButton(
-                                onPressed: () async {
-                                  bool confirmarEliminacion = await showDialog(
-                                      context: context,
-                                      builder: (context) {
-                                        return AlertDialog(
-                                          title: Center(
-                                              child: Text("Eliminar cliente")),
-                                          content: Column(
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              Center(
-                                                  child: Text(
-                                                      "¿Seguro que quieres eliminar este cliente?")),
-                                            ],
-                                          ),
-                                          actions: <Widget>[
-                                            Center(
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceEvenly,
-                                                children: [
-                                                  TextButton(
-                                                    child: Text("Cancelar"),
-                                                    onPressed: () {
-                                                      Navigator.of(context)
-                                                          .pop(false);
-                                                    },
-                                                  ),
-                                                  TextButton(
-                                                    child: Text("Confirmar"),
-                                                    onPressed: () {
-                                                      Navigator.of(context)
-                                                          .pop(true);
-                                                    },
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                          ],
-                                        );
-                                      });
-
-                                  if (confirmarEliminacion == true) {
-                                    bool eliminado =
-                                        await _deleteClient(clientId);
-                                    if (eliminado) {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(const SnackBar(
-                                              content: Text(
-                                                  'Cliente eliminado correctamente'),
-                                              backgroundColor: Colors.green));
-                                    } else {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(const SnackBar(
-                                        content: Text(
-                                            'Error al eliminar el cliente'),
-                                        backgroundColor: Colors.red,
-                                      ));
-                                    }
-                                  }
-                                },
-                                icon: const Icon(Icons.delete),
-                              ),
-                            ],
-                          ),
-                        );
-                      });
-                }),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterClient()),
-                );
-              },
-              child: Text('Nuevo cliente'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<bool> _editClient(int clientId, String name, String phone,
-      String comments, String money) async {
-    try {
-      await client.from('clientes').update({
-        'nombre': name,
-        'telefono': phone,
-        'comentario': comments
-      }).eq('id', clientId);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  Future<bool> _deleteClient(String clientId) async {
-    try {
-      await client.from('clientes').delete().eq('id', clientId);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-/*
-  Future<List<Cliente>> _fetchClientes() async {
-    final response = await client
-        .from('clientes')
-        .select()
-        .ilike('nombre', '%${_searchController.text}%')
-        .execute();
-
-    if (response.error != null) {
-      throw Exception('Error fetching clientes: ${response.error!.message}');
-    }
-
-    final List data = response.data as List;
-    return data.map((json) => Cliente.fromJson(json)).toList();
-  }
-
-
-}
-*/
-/*class Cliente {
-  final int id;
-  final String nombre;
-  final String apellidos;
-
-  Cliente({required this.id, required this.nombre, required this.apellidos});
-
-  factory Cliente.fromJson(Map<String, dynamic> json) {
-    return Cliente(
-      id: json['id'],
-      nombre: json['nombre'],
-      apellidos: json['apellidos'],
-    );
-  }
-}*/
-}
-*/
