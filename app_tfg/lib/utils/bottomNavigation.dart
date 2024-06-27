a379d09139e2be3b8b9f21057cf453647927ed2e
Julia Cano <juliacanoflor@gmail.com>
2024-06-27 16:10:12 +0200

    primera iteracion documentacion

--- app_tfg/lib/utils/bottomNavigation.dart
+++ app_tfg/lib/utils/bottomNavigation.dart
@@ -17,6 +17,128 @@ class _BottomNavState extends State<BottomNav> {
     MainPage(),
     ClientView(),
     CategoryView(),
+    // Add other screens here
+  ];
+
+  void _onItemTapped(int index) async {
+    if (index == 5) {
+      bool? confirmLogout = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Center(child: Text("Cerrar sesión")),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Center(child: Text("¿Seguro que quieres cerrar sesión?")),
+              ],
+            ),
+            actions: <Widget>[
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextButton(
+                      child: Text("Cancelar"),
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                    ),
+                    TextButton(
+                      child: Text("Confirmar"),
+                      onPressed: () {
+                        Navigator.of(context).pop(true);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      );
+
+      if (confirmLogout == true) {
+        await client.auth.signOut();
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => Login()),
+          );
+        }
+      }
+    } else {
+      if (mounted) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.swap_horiz),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+
+/*import 'package:flutter/material.dart';
+import '../mainpage.dart';
+import '../login.dart';
+import '../clientView.dart';
+import '../articles/categoryView.dart';
+import 'common.dart';
+
+class BottomNav extends StatefulWidget {
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    MainPage(),
+    ClientView(),
+    CategoryView(),
 
     //Login(),
     // Add other screens here
@@ -117,3 +239,4 @@ class _BottomNavState extends State<BottomNav> {
     );
   }
 }
+*/
