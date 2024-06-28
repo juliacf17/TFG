5fbc57e580db720d2c7dc2cad98f093793e0c4f8
Julia Cano <juliacanoflor@gmail.com>
2024-06-28 20:03:39 +0200

    lista stock fea

--- app_tfg/lib/articles/articlesView.dart
+++ app_tfg/lib/articles/articlesView.dart
@@ -4,6 +4,392 @@ import '../utils/common.dart';
 import 'newArticle.dart';
 import 'editArticle.dart';
 import 'articleDetails.dart';
+import 'stock.dart';
+
+class ArticleView extends StatelessWidget {
+  final String categoryId;
+
+  ArticleView({required this.categoryId});
+
+  @override
+  Widget build(BuildContext context) {
+    return ArticleScreen(categoryId: categoryId);
+  }
+}
+
+class ArticleScreen extends StatefulWidget {
+  final String categoryId;
+
+  ArticleScreen({required this.categoryId});
+
+  @override
+  _ArticleScreenState createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends State<ArticleScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  Stream<List<Map<String, dynamic>>>? articleStream;
+  List<String> uniqueSubcategories = [];
+
+  String subcategoryFilter = '';
+  bool actualizarDesplegable = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchArticles();
+  }
+
+  Future<void> fetchArticles() async {
+    final articles = await client
+        .from('articulos')
+        .select()
+        .eq('categoriaId', widget.categoryId);
+
+    setState(() {
+      articleStream = Stream.value(articles as List<Map<String, dynamic>>);
+
+      articles.forEach((article) {
+        final String? subcategoria = article[
+            'subcategoria']; // Asegúrate de que 'subcategoria' exista en tu estructura de datos
+        if (subcategoria != null &&
+            subcategoria.isNotEmpty &&
+            !uniqueSubcategories.contains(subcategoria)) {
+          uniqueSubcategories.add(subcategoria);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    dynamic articles;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Nuestros Artículos'),
+      ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 3,
+                      child: TextField(
+                        controller: _searchController,
+                        decoration: InputDecoration(
+                          labelText: 'Buscar',
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(),
+                        ),
+                        onChanged: (value) {
+                          setState(() {});
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 10), // Añadir espacio entre los widgets
+                    Flexible(
+                      flex: 1,
+                      child: DropdownButtonFormField<String>(
+                        items: [
+                          DropdownMenuItem(
+                            value: '', // Valor vacío para deseleccionar
+                            child: Text('Dejar blanco'),
+                          ),
+                          ...uniqueSubcategories
+                              .map((subcategory) => DropdownMenuItem(
+                                    value: subcategory,
+                                    child: Text(subcategory),
+                                  )),
+                        ],
+                        onChanged: (value) {
+                          setState(() {
+                            subcategoryFilter = value!;
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          labelText: 'Subcategoría',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 15.0),
+                Expanded(
+                  child: StreamBuilder<List<Map<String, dynamic>>>(
+                    stream: articleStream,
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      articles = snapshot.data!;
+                      final searchQuery = _searchController.text.toLowerCase();
+
+                      // Filtrar artículos según la búsqueda
+                      final filteredArticles = articles.where((article) {
+                        final articleName = article['nombre'].toLowerCase();
+                        final matchesSearchQuery =
+                            articleName.contains(searchQuery);
+
+                        final articleSubcategory = article['subcategoria'];
+                        final matchesSubcategory = subcategoryFilter.isEmpty ||
+                            articleSubcategory == subcategoryFilter;
+
+                        return matchesSearchQuery && matchesSubcategory;
+                      }).toList();
+
+                      // Ordenar los artículos por nombre
+                      filteredArticles.sort((a, b) => a['nombre']
+                          .toString()
+                          .toLowerCase()
+                          .compareTo(b['nombre'].toString().toLowerCase()));
+
+                      // Obtener subcategorías únicas
+                      filteredArticles.forEach((article) {
+                        final String? subcategoria = article[
+                            'subcategoria']; // Asegúrate de que 'subcategoria' exista en tu estructura de datos
+                        if (subcategoria != null &&
+                            subcategoria.isNotEmpty &&
+                            !uniqueSubcategories.contains(subcategoria)) {
+                          uniqueSubcategories.add(subcategoria);
+                        }
+                      });
+
+                      uniqueSubcategories.sort(
+                          (a, b) => a.toLowerCase().compareTo(b.toLowerCase()));
+
+                      return ListView.builder(
+                        itemCount: filteredArticles.length,
+                        itemBuilder: (context, index) {
+                          final article = filteredArticles[index];
+                          final articleId = article['id'].toString();
+                          final articleName = article['nombre'];
+                          final articlePrecio =
+                              article['precio'].toStringAsFixed(2);
+
+                          return GestureDetector(
+                            onTap: () async {
+                              final result = await Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ArticleDetailsScreen(
+                                      categoryId: widget.categoryId,
+                                      existingSubcategories:
+                                          uniqueSubcategories,
+                                      articleId: articleId),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.black, width: 1.2),
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              margin: EdgeInsets.symmetric(vertical: 2.0),
+                              child: ListTile(
+                                title: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      articleName,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(height: 5.0),
+                                    Text('Precio: $articlePrecio €'),
+                                  ],
+                                ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () async {
+                                        final result = await Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                EditArticleScreen(
+                                                    categoryId:
+                                                        widget.categoryId,
+                                                    existingSubcategories:
+                                                        uniqueSubcategories,
+                                                    articleId: articleId),
+                                          ),
+                                        );
+
+                                        // Actualizar el stream si se editó el artículo
+                                        if (result == true) {
+                                          fetchArticles();
+                                        }
+                                      },
+                                      icon: const Icon(Icons.edit),
+                                    ),
+                                    IconButton(
+                                      onPressed: () async {
+                                        bool confirmarEliminacion =
+                                            await showDialog(
+                                          context: context,
+                                          builder: (context) {
+                                            return AlertDialog(
+                                              title: Center(
+                                                  child: Text(
+                                                      "Eliminar artículo")),
+                                              content: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  Center(
+                                                      child: Text(
+                                                          "¿Seguro que quieres eliminar este artículo?")),
+                                                ],
+                                              ),
+                                              actions: <Widget>[
+                                                Center(
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceEvenly,
+                                                    children: [
+                                                      TextButton(
+                                                        child: Text("Cancelar"),
+                                                        onPressed: () {
+                                                          Navigator.of(context)
+                                                              .pop(false);
+                                                        },
+                                                      ),
+                                                      TextButton(
+                                                        child:
+                                                            Text("Confirmar"),
+                                                        onPressed: () {
+                                                          Navigator.of(context)
+                                                              .pop(true);
+                                                        },
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+
+                                        if (confirmarEliminacion == true) {
+                                          bool eliminado =
+                                              await _deleteArticle(articleId);
+                                          if (eliminado) {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                content: Text(
+                                                    'Artículo eliminado correctamente'),
+                                                backgroundColor: Colors.green,
+                                              ),
+                                            );
+                                          } else {
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(
+                                              const SnackBar(
+                                                content: Text(
+                                                    'Error al eliminar el artículo'),
+                                                backgroundColor: Colors.red,
+                                              ),
+                                            );
+                                          }
+                                        }
+                                      },
+                                      icon: const Icon(Icons.delete),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => StockRenewalScreen(
+                      categoryId: widget.categoryId,
+                      articles: articles,
+                    ),
+                  ),
+                );
+              },
+              child: Text('Renovar stock'),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => NewArticle(
+                      categoryId: widget.categoryId,
+                      existingSubcategories: uniqueSubcategories,
+                    )),
+          );
+
+          if (result == true) {
+            fetchArticles(); // Actualiza la lista de artículos después de añadir uno nuevo
+          }
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
+  Future<bool> _deleteArticle(String articleId) async {
+    try {
+      await client.from('tallas').delete().eq('articuloId', articleId);
+
+      await client.from('articulos').delete().eq('id', articleId);
+
+      setState(() {
+        fetchArticles(); // Asegurar que el widget se reconstruya con el nuevo stream
+      });
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+}
+
+
+
+/*import 'package:flutter/material.dart';
+
+import '../utils/common.dart';
+import 'newArticle.dart';
+import 'editArticle.dart';
+import 'articleDetails.dart';
 
 class ArticleView extends StatelessWidget {
   final String categoryId;
@@ -352,3 +738,4 @@ class _ArticleScreenState extends State<ArticleScreen> {
     }
   }
 }
+*/
---
+++ app_tfg/lib/articles/stock.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import '../utils/common.dart';
+
+class StockRenewalScreen extends StatelessWidget {
+  final String categoryId;
+  final List<Map<String, dynamic>> articles;
+
+  StockRenewalScreen({required this.categoryId, required this.articles});
+
+  Future<List<Map<String, dynamic>>> getLowStockArticles() async {
+    List<Map<String, dynamic>> lowStockArticles = [];
+
+    for (var article in articles) {
+      final articleId = article['id'];
+
+      final responseData =
+          await client.from('tallas').select().eq('articuloId', articleId);
+
+      //final response = responseData.data as List<Map<String, dynamic>>;
+
+      if (responseData.isNotEmpty) {
+        for (var talla in responseData) {
+          if (talla['cantidadActual'] <= talla['cantidadMinima']) {
+            lowStockArticles.add({
+              'nombre': article['nombre'],
+              'talla': talla['talla'],
+              'color': talla['color'],
+              'cantidad_actual': talla['cantidadActual'],
+              'cantidad_minima': talla['cantidadMinima'],
+            });
+          }
+        }
+      }
+    }
+
+    return lowStockArticles;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista renovación stock'),
+      ),
+      body: FutureBuilder<List<Map<String, dynamic>>>(
+        future: getLowStockArticles(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final lowStockArticles = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: lowStockArticles.length,
+            itemBuilder: (context, index) {
+              final article = lowStockArticles[index];
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: TextFormField(
+                        initialValue: article['nombre'],
+                        decoration: InputDecoration(
+                          labelText: 'Nombre artículo',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        initialValue: article['talla'],
+                        decoration: InputDecoration(
+                          labelText: 'Talla',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        initialValue: article['color'],
+                        decoration: InputDecoration(
+                          labelText: 'Color',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        initialValue: article['cantidad_actual'].toString(),
+                        decoration: InputDecoration(
+                          labelText: 'Cantidad actual',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      flex: 1,
+                      child: TextFormField(
+                        initialValue: article['cantidad_minima'].toString(),
+                        decoration: InputDecoration(
+                          labelText: 'Cantidad mínima',
+                          border: OutlineInputBorder(),
+                        ),
+                        readOnly: true,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
